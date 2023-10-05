79e036ac1a03b675204d02a2ce1ae187664bec6f
Kent C. Dodds <me+github@kentcdodds.com>
2023-10-04 23:12:44 -0600

    types are not messed up anymore

--- exercises/11.test-db/03.problem.isolated-db/vitest.config.ts
+++ exercises/11.test-db/03.problem.isolated-db/vitest.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	// @ts-expect-error - vite types are messed up
 	plugins: [react()],
 	css: { postcss: { plugins: [] } },
 	test: {
